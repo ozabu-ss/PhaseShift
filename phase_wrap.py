bcc26e7ca66e8cf8f225197a08132655bde5bcfe
Oles Zaburannyi <ozabu@softserveinc.com>
2019-10-01 01:51:25 +0300

    m

--- phase_wrap.py
+++ phase_wrap.py
@@ -1,56 +1,44 @@
-from mpl_toolkits.mplot3d import Axes3D
-import matplotlib.pyplot as plt
 from matplotlib import cm
-from matplotlib.ticker import LinearLocator, FormatStrFormatter, MaxNLocator
+from matplotlib import pyplot as plt
+from matplotlib.ticker import MaxNLocator
+from mpl_toolkits.mplot3d import Axes3D  # <-- required by subplot projection
 import numpy as np
 
 
-# Create phase function.
-X = np.arange(-2, 2, 0.05)
-Y = np.arange(-2, 2, 0.05)
-X, Y = np.meshgrid(X, Y)
-R = X*np.exp(-X**2 - Y**2)
-Z = np.sin(R) * 50  + np.random.random(X.shape) * 2
+def calculate_something(X, Y):
+    # Create phase function.
+    R = X * np.exp(-X ** 2 - Y ** 2)
+    Z = np.sin(R) * 50 + np.random.random(X.shape) * 2
 
-def wrap(z):
-	while np.abs(z) > np.pi:
-		z = z - np.sign(z) * 2*np.pi
-	return z
+    wrap = np.vectorize(lambda z: (z + np.pi) % (2 * np.pi) - np.pi)
 
-def array_for(z):
-	return np.array([wrap(zi) for zi in z])
+    Z_wrapped = wrap(Z)
+    shift_x, shift_y = np.roll(Z_wrapped, 1, axis=0), np.roll(Z_wrapped, 1, axis=1)
+    delta_x, delta_y = wrap(Z_wrapped - shift_x), wrap(Z_wrapped - shift_y)
 
-Z_wrapped = np.array(map(array_for, Z))
-delta_x = np.diff(Z_wrapped, axis = 0)
-delta_y = np.diff(Z_wrapped, axis = 1)
-delta_x = np.array(map(array_for, delta_x))
-delta_y = np.array(map(array_for, delta_y))
+    b = (delta_x + delta_y + shift_x + shift_y - 2 * Z_wrapped) / (2 * np.pi)
 
-b = np.zeros(Z.shape)
-for i in range(1, Z.shape[0]):
-	for j in range(1, Z.shape[1]):
-		b[i][j] = delta_x[i-1][j] + delta_y[i][j-1] - 2 * Z_wrapped[i][j] + Z_wrapped[i-1][j] + Z_wrapped[i][j-1]
-		b[i][j] = b[i][j] / (2 * np.pi)
+    k = np.zeros(Z.shape)
+    for i in range(1, Z.shape[0]):
+        for j in range(1, Z.shape[1]):
+            k[i][j] = (b[i][j] + k[i - 1][j] + k[i][j - 1]) / 2
 
-#compute k
-k = np.zeros(Z.shape)
-for i in range(1, Z.shape[0]):
-	for j in range(1, Z.shape[1]):
-		k[i][j] = (b[i][j] + k[i-1][j] + k[i][j-1])/2
+    Z_unwrapped = Z_wrapped + 2 * np.pi * np.round(k)
 
-#compute unwrapped Z
-Z_unwrapped = Z_wrapped + 2*np.pi*np.round(k)
+    return Z, Z_unwrapped, Z_wrapped
 
-# Plot the surfaces
-fig = plt.figure(figsize=plt.figaspect(0.5))
-ax = fig.add_subplot(1, 3, 1, projection='3d')
-surf = ax.plot_surface(X, Y, Z, cmap=cm.coolwarm,
-                       linewidth=0, antialiased=False)
-levels = MaxNLocator(nbins=15).tick_values(Z_wrapped.min(), Z_wrapped.max())
-ax = fig.add_subplot(1, 3, 2)
-surf = ax.contourf(X, Y, Z_wrapped, cmap=cm.coolwarm,
-                       levels = levels)
-ax = fig.add_subplot(1, 3, 3, projection='3d')
-surf = ax.plot_surface(X, Y, Z_unwrapped, cmap=cm.coolwarm,
-                       linewidth=0, antialiased=False)
-plt.show()
+
+if __name__ == '__main__':
+    X, Y = np.meshgrid(np.arange(-2, 2, 0.05), np.arange(-2, 2, 0.05))
+    Z, Z_unwrapped, Z_wrapped = calculate_something(X, Y)
+
+    # Plot the surfaces
+    fig = plt.figure(figsize=plt.figaspect(0.5))
+    ax = fig.add_subplot(1, 3, 1, projection='3d')
+    surf = ax.plot_surface(X, Y, Z, cmap=cm.coolwarm, linewidth=0, antialiased=False)
+    levels = MaxNLocator(nbins=15).tick_values(Z_wrapped.min(), Z_wrapped.max())
+    ax = fig.add_subplot(1, 3, 2)
+    surf = ax.contourf(X, Y, Z_wrapped, cmap=cm.coolwarm, levels=levels)
+    ax = fig.add_subplot(1, 3, 3, projection='3d')
+    surf = ax.plot_surface(X, Y, Z_unwrapped, cmap=cm.coolwarm, linewidth=0, antialiased=False)
+    plt.show()
